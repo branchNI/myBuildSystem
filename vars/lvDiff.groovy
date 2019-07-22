c2237e456f66f0a7882751a87b808c1cfa730762
BranchNI <brandon.nguyen@ni.com>
2019-07-22 18:34:50 -0500

    Changed lvdiff path

--- vars/lvDiff.groovy
+++ vars/lvDiff.groovy
@@ -7,7 +7,7 @@ def call(lvVersion, lvBitness) {
 
 	echo 'Running LabVIEW diff build between origin/master and this commit'
 
-	bat "python -u \"${stepsDir}\\labview_diff.py\" \"${operationsDir}\" \"${diffDir}\" ${lvVersion} \{lvBitness} --target=origin/master"
+	bat "python -u \"${stepsDir}\\labview_diff.py\\" \"${operationsDir}\\\" \"${diffDir}\" ${lvVersion} \{lvBitness} --target=origin/master"
     
 	bat "python -u \"${stepsDir}\\github_commenter.py\" --token=\"${ACCESS_TOKEN}\" --pic-dir=\"${diffDir}\" --pull-req=\"${env.CHANGE_ID}\" --info=\"${ORG_NAME}/${repo}/${env.CHANGE_ID}\" --pic-repo=\"${ORG_NAME}/${PIC_REPO}\""
 }
