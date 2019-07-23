26d02f9695744ac34a010c31ad088492c88b57a0
BranchNI <brandon.nguyen@ni.com>
2019-07-22 19:00:16 -0500

    groovy edits

--- vars/lvDiff.groovy
+++ vars/lvDiff.groovy
@@ -1,13 +1,13 @@
 def call(lvVersion, lvBitness) {
 	def diffDir = "${WORKSPACE}\\DIFFDIR"
 	def stepsDir = "${WORKSPACE}\\${BUILD_SYSTEM_REPO}\\steps"
-	def operationsDir = "${WORKSPACE}\\${BUILD_SYSTEM_REPO}\\lv\\operations\\"
+	def operationsDir = "${WORKSPACE}\\${BUILD_SYSTEM_REPO}\\lv\\operations"
 	def prNum = env.CHANGE_ID
 	def repo = getComponentParts()['repo']
 
 	echo 'Running LabVIEW diff build between origin/master and this commit'
 
-	bat "python -u \"${stepsDir}\\labview_diff.py\" \"${operationsDir}\\\" \"${diffDir}\\\" ${lvVersion} ${lvBitness} --target=origin/master"
+	bat "python -u \"${stepsDir}\\labview_diff.py\" \"${operationsDir}\\\\\" \"${diffDir}\\\\\" ${lvVersion} ${lvBitness} --target=origin/master"
     
 	bat "python -u \"${stepsDir}\\github_commenter.py\" --token=\"${ACCESS_TOKEN}\" --pic-dir=\"${diffDir}\" --pull-req=\"${env.CHANGE_ID}\" --info=\"${ORG_NAME}/${repo}/${env.CHANGE_ID}\" --pic-repo=\"${ORG_NAME}/${PIC_REPO}\""
 }
